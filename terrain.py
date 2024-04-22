9e1ba5c0dafb853b3333210069073ac62d7994d9
Louis Declerck <84902823+Louis2675@users.noreply.github.com>
2024-04-22 10:55:25 +0000

    generate terrain started add right and bottom influence +

--- terrain.py
+++ terrain.py
@@ -3,10 +3,11 @@ Fichier contenant la classe grid définissant une grid de jeu.
 """
 
 class Cell:
-    def __init__(self, terrain_type, is_house, fire_strength):
+    def __init__(self, terrain_type, is_house, fire_strength, height):
         self.terrain_type = terrain_type
         self.is_house = is_house
         self.fire_strength = fire_strength
+        self.height = height
 
 
 class Terrain:
@@ -16,13 +17,17 @@ class Terrain:
         for i in range (size):
             self.grid.append([])
             for _ in range (size) : 
-                self.grid[i].append(Cell("None", False, 0))
+                self.grid[i].append(Cell("None", False, 0, 0))
     
     def display_grid(self):
         for line in range(self.size):
             print()
             for col in range(self.size):
-                cell = self.grid[line][col]
+                if self.grid[line][col].height <= 0:
+                    print("\033[1;36m{}\033[1;37m".format(self.grid[line][col].height), end="")
+                else:
+                    print("\033[1;32m{}\033[1;37m".format(self.grid[line][col].height), end="")
+
                 
                 
                 
