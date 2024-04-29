42532e114a55619f510896b1a89456c343cdc1e1
martinppx <martin.af.leleux@gmail.com>
2024-04-29 11:29:26 +0200

    Work done on the display, add the instructions as a pdf

--- display.py
+++ display.py
@@ -1,5 +1,11 @@
 import pygame
-pygame.init() 
+
+# initializing pygame
+pygame.font.init()
+
+# check whether font is initialized
+# or not
+pygame.font.get_init()
 
 window = pygame.display.set_mode((1000,1000))
 
@@ -7,24 +13,43 @@ running  = True
 
 color = (255,255,255)
 
+font1 = pygame.font.SysFont('freesanbold.ttf', 50)
+
+# The title for the sim
+text1 = font1.render('Welcome to the fire hazard simulator', True, (0, 0, 0))
+
+# create a rectangular object for the title
+textRect1 = text1.get_rect()
+
+# setting center for the title
+textRect1.center = (500, 100)
+
+# We create the first image
+image = pygame.image.load('images/sun.png')
+
+imagerect = image.get_rect()
+
+imagerect.center = (500, 500)
+
+# Title of our window
 pygame.display.set_caption('Fire Hazard Sim')
 
 while running:
+
     for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
+        if event.type == pygame.QUIT :  
             running = False
+        
+        elif event.type == pygame.KEYDOWN:
+            
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     window.fill(color)
 
-    image = pygame.image.load('images/sun.png')
+    window.blit(text1, textRect1)
 
-    window.blit(image,(1,1))
-
-    pygame.time.wait(1000)
-
-    ticks=pygame.time.get_ticks()
-
-    print(ticks)
+    window.blit(image, imagerect)
 
     pygame.display.flip()
 
